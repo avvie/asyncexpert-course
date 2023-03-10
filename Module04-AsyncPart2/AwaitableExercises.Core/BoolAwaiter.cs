4f12be44fab314752d789f962b7b45f6f8ed7cc2
Pandora Konofagou <pandora.konof@gmail.com>
2023-03-10 07:59:22 +0200

    module 4 excersices

--- Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
+++ Module04-AsyncPart2/AwaitableExercises.Core/BoolAwaiter.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace AwaitableExercises.Core
 {
     public static class BoolExtensions
     {
+        public static BoolAwaiter GetAwaiter(this bool value) => new BoolAwaiter(value);
     }
 
     public class BoolAwaiter : INotifyCompletion
     {
-        public void OnCompleted(Action continuation)
-        {
-            throw new NotImplementedException();
-        }
+        private readonly bool value;
+        public BoolAwaiter(bool value) => this.value = value;
+        
+        public bool IsCompleted  => true;
+        public void OnCompleted(Action continuation) { }
+        public bool GetResult() => value;
     }
 }
